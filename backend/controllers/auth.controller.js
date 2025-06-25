c10a83f2dec45051dd04984ef806fe269c388bd9
Norbert <norbertn397@gmail.com>
2025-06-25 12:55:28 +0200

    1-project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+  res.json({
+    data: "You are on signup page",
+  });
+};
+
+export const login = async (req, res) => {
+  res.json({
+    data: "You are on signup page",
+  });
+};
+
+export const logout = async (req, res) => {
+  res.json({
+    data: "You are on signup page",
+  });
+};
